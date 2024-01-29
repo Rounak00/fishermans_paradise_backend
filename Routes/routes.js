5c36e5486a668ba95b5e2a57699fedf87d54b5b1
Rounak00 <mukherjeerounak10@gmail.com>
2024-01-29 18:16:50 +0530

    git push

--- Routes/routes.js
+++ Routes/routes.js
@@ -4,6 +4,18 @@ const healthCheckController =require("../controller/healthCheck");
 //Health Check
 routes.get("/healthCheck",healthCheckController.healthCheck);
 
-
+//authentication
+routes.post("/admin/login",)
+routes.post("/admin/register",)
+routes.post("/fisherman/login",)
+routes.post("/fisherman/register",)
+routes.post("/customer/login",)
+routes.post("/customer/register",)
+//admin -  profile, approve of fisherman
+//fisherman - add item, delete item, update item, profile
+//customer -  user profile, 
+//search
+//order
+//cart
 
 module.exports=routes;
